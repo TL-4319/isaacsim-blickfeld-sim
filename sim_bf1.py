09e0d2727f7782f113ec10d905f79a60239fdd5a
Tuan Luong <tdluong@crimson.ua.edu>
2025-02-24 22:36:04 -0600

    add function to generate scan pattern

--- sim_bf1.py
+++ sim_bf1.py
@@ -3,6 +3,30 @@
 # 2025/02/24
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
+
+class ScanPattern(object):
+    frame_rate_hz = 0
+    frame_period_s = 0
+    num_point_per_line = 0
+    num_scanline = 0
+    total_point = 0
+    time_vec_ns = np.array([])
+    az_vec_deg = np.array([])
+    el_vec_deg = np.array([])
+
+    def __init__(self, frame_period_s, total_point, num_scanline, time_vec_ns, az_vec_deg, el_vec_deg):
+        self.frame_period_s = frame_period_s
+        self.frame_rate_hz = 1/frame_period_s
+        self.num_scanline = num_scanline
+        self.total_point = total_point
+        self.num_point_per_line = total_point / num_scanline
+        self.time_vec_ns = time_vec_ns
+        self.az_vec_deg = az_vec_deg
+        self.el_vec_deg = el_vec_deg
+
+
+
 
 def parse_arg():
     # Expose as much real life configuration from Blickfeld Cube1 as posible along with emulate real-life constraint
@@ -16,7 +40,7 @@ def parse_arg():
 The output file is labeled BF1_<FREQ>_<HOR_MEAS_FOV>_<VER_MEAS_FOV>_<HOR_ANG_RES>_<NUM_SCAN_UP>_<NUM_SCAN_DOWN>.json",
                                      formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument('-f', '--freq', type=int, default=250, help='Mirrors eigen frquency. Defaults to 250 Hz.')
-    parser.add_argument('--hor_meas_fov', type=int, default=72, help='Horizontal measurement FOV in degrees. Defaults to 72 deg [1 - 72]')
+    parser.add_argument('--hor_meas_fov', type=int, default=70, help='Horizontal measurement FOV in degrees. Defaults to 70 deg [1 - 72]')
     parser.add_argument('--ver_meas_fov', type=int, default=30, help='Vertical measuremetn FOV in degrees. Defaults to 30 deg [1 - 30]')
     parser.add_argument('--hor_ang_res', type=int, default=4, help='Horizontal angular resolution of measurements in deci degrees. Defaults to 4 deci deg [4 - 10] ')
     parser.add_argument('--num_scan_up', type=int, default=200, help='Number of scanlines in the up ramp phase. Default is 200 [1 - 200]')
@@ -47,24 +71,15 @@ def is_valid_params(lidar_params):
     return True
 
 def gen_scan_pattern(lidar_params):
-    # Return arrays of size 3xN where N is the number of points in the pattern.
-    # The information encodes in the rows are
-    # Row 1 - offset from start of frame in nano second
-    # Row 2 - azimuth in radian
-    # Row 3 - elevation in radian
+    # Pre compute some constants
     HOR_MIR_FOV_MDEG = 80 * 1000 # Mirror FOV. Is not configurable
     hor_ang_res_mdeg = lidar_params.hor_ang_res * 100
-    hor_meas_fov_mdeg = lidar_params.hor_meas_fov * 1000
-    ver_meas_fov_mdeg = lidar_params.hor_meas_fov * 1000
-    
-    # Generate mirror ramp functions
     tot_num_scanline = lidar_params.num_scan_up + lidar_params.num_scan_down
     ramp_transition = lidar_params.num_scan_up / tot_num_scanline
     period_per_scanline_s = 1/lidar_params.freq
-    frame_period_s = tot_num_scanline * period_per_scanline_s
-    num_point_per_scanline = int(hor_meas_fov_mdeg / hor_ang_res_mdeg)
-    total_num_point = num_point_per_scanline * tot_num_scanline
+    frame_period_s = tot_num_scanline * period_per_scanline_s/2
 
+    # Generate a time vector of points as they are equidistant in angle, not time
     angle_to_trig_down_mdeg = np.arange(HOR_MIR_FOV_MDEG/2, -HOR_MIR_FOV_MDEG/2, -hor_ang_res_mdeg)
 
     time_vec_half_period = np.arccos(2 * angle_to_trig_down_mdeg / HOR_MIR_FOV_MDEG) / (2 * np.pi * lidar_params.freq)
@@ -75,8 +90,36 @@ def gen_scan_pattern(lidar_params):
         time_vec_s = np.concatenate((time_vec_s,0.5 * ii * period_per_scanline_s + time_vec_half_period))
     time_vec_ns = np.round(time_vec_s * 1000000000)
 
-    print(time_vec_ns.size)
-    print(time_vec_ns)
+    # Mirror movement function
+    # Horizontal mirror
+    hor_ang_deg = np.round(40 * np.cos(2 * np.pi * lidar_params.freq * time_vec_s),2)
+
+    # Vertical mirror
+    ramp_func = np.zeros_like(hor_ang_deg)
+    ramp_transition_time = ramp_transition * frame_period_s
+
+    # Line intercept of the down ramp amplitude function
+    slope = -1 / (frame_period_s - ramp_transition_time)
+    c = frame_period_s / (frame_period_s - ramp_transition_time)
+    for ii in range(ramp_func.size):
+        cur_t = time_vec_s[ii]
+        if cur_t <= ramp_transition_time:
+            # Ramp up
+            ramp_func[ii] = 1/ramp_transition_time * cur_t
+        else:
+            ramp_func[ii] = slope * cur_t + c
+    
+    ver_ang_deg = np.round((0.5 * lidar_params.ver_meas_fov * np.sin(2 * np.pi * lidar_params.freq * time_vec_s)) * ramp_func,2)
+
+    # Remove entris outside the horizontal meas FOV
+    ind_out_FOV = np.flatnonzero(np.abs(hor_ang_deg) > lidar_params.hor_meas_fov/2)
+    time_vec_ns = np.delete(time_vec_ns, ind_out_FOV)
+    hor_ang_deg = np.delete(hor_ang_deg, ind_out_FOV)
+    ver_ang_deg = np.delete(ver_ang_deg, ind_out_FOV)
+
+    num_point = ver_ang_deg.size
+
+    return ScanPattern(frame_period_s, num_point, tot_num_scanline, time_vec_ns, hor_ang_deg, ver_ang_deg)
     
 
 
@@ -91,6 +134,6 @@ if __name__ == "__main__":
     if not is_valid_params(lidar_params):
         exit()
 
-    gen_scan_pattern(lidar_params)
+    scan_pattern = gen_scan_pattern(lidar_params)
 
-    print(output_json_filename)
+    print(scan_pattern.time_vec_ns)
